b9ca424e08ecd7cd6ed55f4320f66c2df24647e3
degenerat3 <james.maskelony@gmail.com>
2020-08-24 22:33:58 -0400

    basic packet send

--- client.go
+++ client.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"log"
+	"net"
+)
+
+func clnt(dstIP string) {
+	// Start con to listen for replies
+	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer c.Close()
+
+	// Resolve any DNS (if used) and get the real IP of the target
+	dst, err := net.ResolveIPAddr("ip4", dstIP)
+	if err != nil {
+		panic(err)
+	}
+
+	// Craft a new echo
+	m := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.RawBody{
+			Data: []byte("Helloxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+		},
+	}
+	b, err := m.Marshal(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = c.WriteTo(b, dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- server.go
+++ server.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"golang.org/x/net/icmp"
+	"log"
+	"time"
+)
+
+func srvr() {
+	conn, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		msg := make([]byte, 2000)
+		err = conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+		if err != nil {
+			log.Fatal(err)
+		}
+		n, peer, err := conn.ReadFrom(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("got: %d \n", n)
+		parsed, err := icmp.ParseMessage(1, msg[:n])
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		bd, err := parsed.Body.Marshal(1)
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		log.Printf("message = '%s', length = %d, source-ip = %s", string(bd), n, peer)
+	}
+}
